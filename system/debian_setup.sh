c964ed168998417fd86ea5a17b4fccbf1f69c8fd
Daniel Pellegrino <danpellegrino1@gmail.com>
2023-12-19 04:49:01 -0500

    updated debian_setup.sh

--- system/debian_setup.sh
+++ system/debian_setup.sh
@@ -118,10 +118,10 @@ main ()
   genfstab -U /mnt >> /mnt/etc/fstab
 
   # Set the apt sources
-  echo "deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware" > /etc/apt/sources.list
-  echo "deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware" >> /etc/apt/sources.list
-  echo "deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware" >> /etc/apt/sources.list
-  echo "deb http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware" >> /etc/apt/sources.list
+  echo "deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware" > /mnt/etc/apt/sources.list
+  echo "deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware" >> /mnt/etc/apt/sources.list
+  echo "deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware" >> /mnt/etc/apt/sources.list
+  echo "deb http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware" >> /mnt/etc/apt/sources.list
 
   echo $HOSTNAME > /mnt/etc/hostname
 
